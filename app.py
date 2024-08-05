9bafef43577dfe84732ccdb0b83d1db102e8b837
Andre <andre.hermanto93@gmail.com>
2024-08-05 19:06:16 +1000

    basic app

--- app.py
+++ app.py
@@ -2,15 +2,24 @@
 
 import os
 
-from flask import Flask, jsonify, request, make_response
-from flask_restful import Resource
-from models import Product, User
-from config import db, api, app
+from flask import Flask, jsonify, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+
+app = Flask(__name__)
+app.json.compact = False
 
 print('Hello World')
+
+api = Api(app)
 
 class Hello(Resource):
     def get(self):
         print('hello world')
+        return 'Hello World'
+
 api.add_resource(Hello, '/')
+
+
 
